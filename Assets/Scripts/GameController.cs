a466b967a4da1737a4cfb428ddcece56cfbff377
04-BryanCheng <abyssgenesis.61@gmail.com>
2021-10-12 16:28:17 +0800

    Added new script and material

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class GameController : MonoBehaviour
+{
+    public int Coincount;
+    public Text scoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Coin")
+        {
+            Destroy(collision.gameObject);
+            Coincount++;
+            scoreText.text = "Score: " + Coincount;
+        }
+    }
+}
