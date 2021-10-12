95c5f171b08122f3b21129f451940a514dd05567
04-BryanCheng <abyssgenesis.61@gmail.com>
2021-10-12 18:28:20 +0800

    Added cursor unlocked after lose or win

--- Assets/Scripts/SceneManament.cs
+++ Assets/Scripts/SceneManament.cs
@@ -13,7 +13,8 @@ public class SceneManament : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
     }
     public void RestartOnClick()
     {
